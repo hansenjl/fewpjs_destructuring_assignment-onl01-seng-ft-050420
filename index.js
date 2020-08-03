a39eebba0c210e7aec053216a718bcf5638cc8fd
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-08-03 05:26:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -55,19 +55,19 @@ const { name, color, song, job, partner} = muppet
 
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
-const {song_2, song_4, k_job, k_partner} = {k_muppet.k_album.the_muppet_movie.song_2, 
+const {song_2, song_4, k_job, k_partner} = {k_muppet.k_album.the_muppet_movie.song_2, k_muppet.k_album.the_muppet_movie.song_4, k_muppet.k_job, k_muppet.k_partner } 
 
-const k_muppet = {
-  k_name: 'Kermit',
-  k_color: 'green',
-  k_album: {
-    the_muppet_movie: {
-      song_1: 'Rainbow Connection',
-      song_2: 'Moving Right Along',
-      song_3: 'Never Before, Never Again',
-      song_4: 'I Hope That Something Better Comes Along',
-    },
-  },
-  k_job: 'Host of The Muppet Show',
-  k_partner: 'Miss Piggy'
-};
+// const k_muppet = {
+//   k_name: 'Kermit',
+//   k_color: 'green',
+//   k_album: {
+//     the_muppet_movie: {
+//       song_1: 'Rainbow Connection',
+//       song_2: 'Moving Right Along',
+//       song_3: 'Never Before, Never Again',
+//       song_4: 'I Hope That Something Better Comes Along',
+//     },
+//   },
+//   k_job: 'Host of The Muppet Show',
+//   k_partner: 'Miss Piggy'
+// };
